6d9d2860d0e2cc78e50863a7999d40ef1a598b77
andreacagnina <amirkal@libero.it>
2024-09-18 17:43:27 +0200

    [feat]done instance category class

--- index.php
+++ index.php
@@ -1,30 +1,37 @@
 <?php
-class PetsProducts
+class PetsProduct
 {
-    public $type;
+    public $title;
     public $categories;
+    public $type;
+    public $price;
 }
 
-class Types
+class Type
 {
     public $food;
     public $toys;
     public $beds;
 }
 
-class Categories
+class Category
 {
-    public $dog;
-    public $cat;
+    public $animal;
+    public $icon;
+
+    function __construct($animal, $icon)
+    {
+        $this->animal = $animal;
+        $this->icon = $icon;
+    }
 }
 
-class Products
-{
-    public $img;
-    public $title;
-    public $price;
-    public $icon_category;
-}
+$dogs = new Category('Dogs', '<i class="fa-solid fa-dog"></i>');
+$cats = new Category('Cats', '<i class="fa-solid fa-cat"></i>');
+
+var_dump($dogs);
+var_dump($cats);
+
 
 ?>
 <!DOCTYPE html>
@@ -33,6 +40,8 @@ class Products
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Shop Online</title>
 </head>
 
