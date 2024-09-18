9ae1e2441f2762cf80c88805dc191d9975fc0829
andreacagnina <amirkal@libero.it>
2024-09-18 17:23:57 +0200

    [upd] scaffolding

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+class PetsProducts
+{
+    public $type;
+    public $categories;
+}
+
+class Types
+{
+    public $food;
+    public $toys;
+    public $beds;
+}
+
+class Categories
+{
+    public $dog;
+    public $cat;
+}
+
+class Products
+{
+    public $img;
+    public $title;
+    public $price;
+    public $icon_category;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shop Online</title>
+</head>
+
+<body>
+    <!-- Immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche:
+L'e-commerce vende prodotti per animali.
+I prodotti sono categorizzati, le categorie sono Cani o Gatti.
+I prodotti saranno oltre al cibo, anche giochi, cucce, etc.Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, prezzo, icona della categoria ed il tipo di articolo che si sta visualizzando (prodotto, cibo, gioco, cuccia).Cominciate a fare prima le classi. Per ogni classe creata, fate una o due istanze per vedere il risultato con il var_dump. Una volta create tutte le classi verificate che siano coerenti con quanto scritto nell'esercizio, allora create delle strutture dati da mostrare in pagina. -->
+
+</body>
+
+</html>
