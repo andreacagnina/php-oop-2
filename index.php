035dd4f4cae28c8615c25e7b0e3fc51ecc0080dc
andreacagnina <amirkal@libero.it>
2024-09-19 02:50:57 +0200

    [feat] add badge

--- index.php
+++ index.php
@@ -24,15 +24,18 @@ class PetsProduct
     public $name;
     public $categories = [];
     public $price;
+    public $pieces;
 
 
-    function __construct($img, $name, array $categories, $price)
+    function __construct($img, $name, array $categories, $price, $pieces)
     {
         $this->img = $img;
         $this->name = $name;
         $this->categories = $categories;
         $this->price = $price;
+        $this->pieces = $pieces;
     }
+
     function getAllCategories()
     {
         $caneEgatto = [];
@@ -41,9 +44,21 @@ class PetsProduct
         }
         return implode(' and ', $caneEgatto);
     }
+
     function getProductDetails()
     {
         return '';
+    }
+
+    public function setPieces($stock)
+    {
+        if ($stock == 0) {
+            return $stock = 'Sold Out';
+        } else if ($stock < 5) {
+            return $stock = 'Few';
+        } else {
+            return $stock = 'In stock';
+        }
     }
 }
 
@@ -53,22 +68,22 @@ class Food extends PetsProduct
     public $ingredients;
     public $weight;
 
-    function __construct($img, $name, array $category, $taste, $ingredients, $weight, $price)
+    function __construct($img, $name, array $category, $taste, $ingredients, $weight, $price, $pieces)
     {
-        parent::__construct($img, $name, $category, $price);
+        parent::__construct($img, $name, $category, $price, $pieces);
         $this->taste = $taste;
         $this->ingredients = $ingredients;
         $this->weight = $weight;
     }
     function getProductDetails()
     {
-        return "<p><strong>Gusto:</strong> {$this->taste}</p>
-                <p class='ovf'><strong>Ingredienti:</strong> {$this->ingredients}</p>
-                <p><strong>Peso:</strong> {$this->weight}</p>";
+        return "<p><strong>Taste:</strong> {$this->taste}</p>
+                <p class='ovf'><strong>Ingredients:</strong> {$this->ingredients}</p>
+                <p><strong>Weight:</strong> {$this->weight}</p>";
     }
 }
 
-$croccantiniAlSalmone = new Food('https://www.robinsonpetshop.it/22977-thickbox_default/amanova-adult-sensitive-salmone-per-cani.jpg', 'Amanova Adult Sensitive Salmone per Cani', [$dogs], 'Salmon', 'Salmone preparato fresco 70% (122 kg di salmone preparato fresco per elaborare 100 kg di prodotto), piselli verdi interi, patata disidratata, proteina di patata, idrolizzato di salmone, lieviti (Saccharomy- ces cerevisiae), fibra di mela, semi di lino (fonte naturale di acidi grassi ω3), minerali, patata dolce, tapioca, olio di pesce (protetto da tocoferoli naturali), glucosamina 1400 mg/kg, kale disidratato, krill antartico, alga kelp, spirulina, cicoria (fonte di FOS) 1000 mg/kg, parete cellulare del lievito idrolizzato (fonte di MOS) 1000 mg/kg, condroitina 1000 mg/kg, zucca disidratata 1000 mg/kg, broccolo disidratato 1000 mg/kg, mirtilli rossi 1000 mg/kg, mirtilli blu 1000 mg/kg, rosmarino 250 mg/kg, yucca schidigera.', '10 KG', '75.99€');
+$croccantiniAlSalmone = new Food('https://www.robinsonpetshop.it/22977-thickbox_default/amanova-adult-sensitive-salmone-per-cani.jpg', 'Amanova Adult Sensitive Salmone per Cani', [$dogs], 'Salmon', 'Salmone preparato fresco 70% (122 kg di salmone preparato fresco per elaborare 100 kg di prodotto), piselli verdi interi, patata disidratata, proteina di patata, idrolizzato di salmone, lieviti (Saccharomy- ces cerevisiae), fibra di mela, semi di lino (fonte naturale di acidi grassi ω3), minerali, patata dolce, tapioca, olio di pesce (protetto da tocoferoli naturali), glucosamina 1400 mg/kg, kale disidratato, krill antartico, alga kelp, spirulina, cicoria (fonte di FOS) 1000 mg/kg, parete cellulare del lievito idrolizzato (fonte di MOS) 1000 mg/kg, condroitina 1000 mg/kg, zucca disidratata 1000 mg/kg, broccolo disidratato 1000 mg/kg, mirtilli rossi 1000 mg/kg, mirtilli blu 1000 mg/kg, rosmarino 250 mg/kg, yucca schidigera.', '10 KG', '75.99€', 0);
 //var_dump($croccantiniAlSalmone);
 
 class Toy extends PetsProduct
@@ -77,22 +92,22 @@ class Toy extends PetsProduct
     public $color;
     public $dimensions;
 
-    function __construct($img, $name, array $category, $material, $color, $dimensions, $price)
+    function __construct($img, $name, array $category, $material, $color, $dimensions, $price, $pieces)
     {
-        parent::__construct($img, $name, $category, $price);
+        parent::__construct($img, $name, $category, $price, $pieces);
         $this->material = $material;
         $this->color = $color;
         $this->dimensions = $dimensions;
     }
     function getProductDetails()
     {
-        return "<p><strong>Materiale:</strong> {$this->material}</p>
-                <p><strong>Colore:</strong> {$this->color}</p>
-                <p><strong>Dimensioni:</strong> {$this->dimensions}</p>";
+        return "<p><strong>Main Material:</strong> {$this->material}</p>
+                <p><strong>Color:</strong> {$this->color}</p>
+                <p><strong>Dimensions:</strong> {$this->dimensions}</p>";
     }
 }
 
-$bacchettaPiuma = new Toy('https://m.media-amazon.com/images/I/61bf1yoHyKL._AC_SL1100_.jpg', 'Giocattolo a bacchetta interattiva in piuma', [$cats], 'Plastic', 'Multicolor', '15 inches', '15€');
+$bacchettaPiuma = new Toy('https://m.media-amazon.com/images/I/61bf1yoHyKL._AC_SL1100_.jpg', 'Giocattolo a bacchetta interattiva in piuma', [$cats], 'Plastic', 'Multicolor', '15 inches', '15€', 30);
 //var_dump($bacchettaPiuma);
 
 class Bed extends PetsProduct
@@ -101,21 +116,21 @@ class Bed extends PetsProduct
     public $color;
     public $dimensions;
 
-    function __construct($img, $name, array $category, $material, $color, $dimensions, $price)
+    function __construct($img, $name, array $category, $material, $color, $dimensions, $price, $pieces)
     {
-        parent::__construct($img, $name, $category, $price);
+        parent::__construct($img, $name, $category, $price, $pieces);
         $this->material = $material;
         $this->color = $color;
         $this->dimensions = $dimensions;
     }
     function getProductDetails()
     {
-        return "<p><strong>Materiale:</strong> {$this->material}</p>
-                <p><strong>Colore:</strong> {$this->color}</p>
-                <p><strong>Dimensioni:</strong> {$this->dimensions}</p>";
+        return "<p><strong>Main Material:</strong> {$this->material}</p>
+                <p><strong>Color:</strong> {$this->color}</p>
+                <p><strong>Dimensions:</strong> {$this->dimensions}</p>";
     }
 }
-$cucciaMorbida = new Bed('https://i0.wp.com/www.zoologos.net/wp-content/uploads/2020/08/htb1ldyjrfzqk1rjszfoq6zfcxxa3.jpg?fit=800%2C800&ssl=1', 'Cuccia Gatto per Gatti e Cani', [$cats, $dogs], 'Cotton', 'Black', '48x48x48 cm', '50€');
+$cucciaMorbida = new Bed('https://i0.wp.com/www.zoologos.net/wp-content/uploads/2020/08/htb1ldyjrfzqk1rjszfoq6zfcxxa3.jpg?fit=800%2C800&ssl=1', 'Cuccia Gatto per Gatti e Cani', [$cats, $dogs], 'Cotton', 'Black', '48x48x48 cm', '50€', 3);
 //var_dump($cucciaMorbida);
 
 $Arrayprodotti = [$croccantiniAlSalmone, $bacchettaPiuma, $cucciaMorbida];
@@ -152,16 +167,18 @@ $Arrayprodotti = [$croccantiniAlSalmone, $bacchettaPiuma, $cucciaMorbida];
                         </div>
                         <hr>
                         <div class="card-body details">
-                            <p><strong>Prezzo:</strong> <?php echo $prodotto->price ?></p>
-                            <p><strong>Categorie:</strong> <?php echo $prodotto->getAllCategories() ?></p>
+                            <p><strong>Price:</strong> <?php echo $prodotto->price ?></p>
+                            <p><strong>Category:</strong> <?php echo $prodotto->getAllCategories() ?></p>
                         </div>
                         <hr>
                         <div class="card-body extra-details">
                             <p><?php echo $prodotto->getProductDetails() ?></p>
                         </div>
                         <hr>
+                        <p class="badge rounded-pill <?php echo $prodotto->pieces == 0 ? 'text-bg-danger' : ($prodotto->pieces < 5 ? 'text-bg-warning' : 'text-bg-success') ?>"><?php echo $prodotto->setPieces($prodotto->pieces) ?></p>
+                        <hr>
                         <div class="card-body sm">
-                            <button type="submit" class="btn btn-primary">Buy Now</button>
+                            <button type="submit" class="btn btn-primary <?php if ($prodotto->pieces == 0) echo 'disabled' ?>">Buy Now</button>
                         </div>
                     </div>
                 </div>
